76b911ce499107da55691c16c3e831d9216c2c38
Varun Banka <bankavarun18@gmail.com>
2023-02-12 01:33:21 +0530

    add file Organizer ffs

--- File Organizer .bat
+++ File Organizer .bat
@@ -1,6 +1,18 @@
 @echo off
+
 title %random% %date% %username% %time% %random%
 color 0a
+
+rem For each file in your folder
+for %%a in (".\*") do (
+rem check if the file has an extension and if it is not our script
+if "%%~xa" NEQ "" if "%%~dpxa" NEQ "%~dpx0" (
+rem check if extension folder exists, if not it is created
+if not exist "%%~xa" mkdir "%%~xa"
+rem Move the file to directory
+move "%%a" "%%~dpa%%~xa\"
+))
+
 ren -=- Writes INFO to a .LOG file in Current Directory -=-
 :info
 cls & color 0a
